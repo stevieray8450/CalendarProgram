70e6bbc2f8ba57ed37f30ab7f31c399cd290fa47
Stephen Boniface <stephen.boniface@smail.rasmussen.edu>
2017-03-01 15:03:08 -0600

    first commit

--- Calendar/Program.cs
+++ Calendar/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Calendar
+namespace CalendarProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DateTime myDT = new DateTime(2017, 3, 1);
+            Calendar myCal = CultureInfo.InvariantCulture.Calendar;
+
+            string currentMonth = myCal.GetMonth(myDT).ToString();
+            string currentDay = myCal.GetDayOfMonth(myDT).ToString();
+            string currentYear = myCal.GetYear(myDT).ToString();
+
+            string currentDate = (currentMonth + "/" + currentDay + "/" + currentYear);
+     
+
+            Console.WriteLine(currentDate);
         }
     }
 }
